1b4470f6ef317a0c907ea991c4a9b4ec205924f2
zhangman <zhang08240@163.com>
2019-06-24 19:11:42 +0800

    效验手机号

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,11 +4,15 @@
         <div class="form-head">
             <img src="./logo_index.png" alt="黑马头条号">
         </div>
-        <el-form class="form-content" ref="form" :model="form">
-  <el-form-item>
+        <el-form
+        class="form-content"
+        ref="form"
+        :model="form"
+        :rules="rules">
+  <el-form-item  prop="mobile">
     <el-input v-model="form.mobile" placeholder="手机号"></el-input>
   </el-form-item>
-  <el-form-item>
+  <el-form-item  prop="code">
     <el-col :span="14">
      <el-input v-model="form.code" placeholder="验证码"></el-input>
     </el-col>
@@ -16,8 +20,12 @@
           <el-button @click="handleSendCode">获取验证码</el-button>
         </el-col>
   </el-form-item>
+  <el-form-item prop="agree">
+   <el-checkbox class="agree-checkbox" v-model="form.agree"></el-checkbox>
+   <span class="agree-text">我已阅读并同意<a href="#">用户协议</a>和 <a href="#">隐私条款</a></span>
+  </el-form-item>
   <el-form-item>
-    <el-button class="btn-login" type="primary" @click="onSubmit">登录</el-button>
+    <el-button class="btn-login" type="primary" @click="handleLogin">登录</el-button>
   </el-form-item>
 </el-form>
 </div>
@@ -32,13 +40,33 @@ export default {
     return {
       form: {
         mobile: '',
-        code: ''
+        code: '',
+        agree: ''
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入正确的手机号', trigger: 'blur' },
+          { len: 11, message: '长度必须为11位', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入正确的验证码', trigger: 'blur' },
+          { len: 6, message: '长度必须为6位', trigger: 'blur' }
+        ],
+        agree: [
+           { required: true, message:'请选择用户协议' },
+           { pattern: /true/, message:'请选择用户协议' }
+        ]
       }
     }
   },
   methods: {
-    onSubmit () {
-      console.log('submit!')
+    handleLogin () {
+      this.$refs['form'].validate(valid => {
+        if (!valid) {
+          return
+        }
+        this.submitLogin()
+      })
     },
     handleSendCode () {
       const { mobile } = this.form
@@ -57,11 +85,43 @@ export default {
           captchaObj.onReady(function () {
             captchaObj.verify()
           }).onSuccess(function () {
-            console.log(captchaObj.getValidate())
+            const {
+              geetest_challenge: challenge,
+              geetest_seccode: seccode,
+              geetest_validate: validate } =
+              captchaObj.getValidate()
+            axios({
+              method: 'GET',
+              url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
+              params: {
+                challenge,
+                validate,
+                seccode
+              }
+            }).then(res => {
+
+            })
           }).onError(function () {
           })
         })
       })
+    },
+    submitLogin () {
+      axios({
+        method: 'POST',
+        url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+        data: this.form
+      })
+        .then(res => {
+          console.log(res.data)
+          this.$message({
+            message: '登录成功',
+            type: 'success'
+          })
+        })
+        .catch((e) => {
+          this.$message.error('手机号或验证码错误!')
+        })
     }
   }
 }
